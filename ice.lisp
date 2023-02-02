cb63bf63ea0ae6194a4d21f98f9cd1136076e5b0
Jan Deinhard <jan.deinhard@dazn.com>
2023-02-02 22:37:12 +0100

    remove obsolete let

--- ice.lisp
+++ ice.lisp
@@ -11,7 +11,7 @@
       (format output "P3~%~d ~d~%255~%" image-width image-height)
       (loop for j from (- image-height 1) downto 0
 	    do (loop for i from 0 below image-width
-		     do (let ((r (/ i (- image-width 1)))
-			      (g (/ j (- image-height 1)))
-			      (b 0.25))
-			  (write-pixel-color output (3d-vectors:vec r g b))))))))
+		     do (let ((pixel-color (3d-vectors:vec (/ i (- image-width 1))
+							   (/ j (- image-height 1))
+							   0.25)))
+			  (write-pixel-color output pixel-color)))))))
