3b44a52807afb1d33f27d2fcf9f0d8806d87d289
Jan Deinhard <jan.deinhard@gmail.com>
2023-02-04 02:13:08 +0100

    add more tests

--- tests/ray.lisp
+++ tests/ray.lisp
@@ -4,6 +4,14 @@
 
 (in-suite :ice/tests)
 
-(test test-ray-at
+(test test-ray-at-1
+  (let ((r (make-instance 'ice::ray :origin (3d-vectors:vec 0 0 0) :direction (3d-vectors:vec 1 2 3))))
+    (is (ice::at r 2) (3d-vectors 2 4 6))))
+
+(test test-ray-at-2
   (let ((r (make-instance 'ice::ray :origin (3d-vectors:vec 1 1 1) :direction (3d-vectors:vec 0 0 1))))
     (is (ice::at r 10) (3d-vectors 1 1 11))))
+
+(test test-ray-at-3
+  (let ((r (make-instance 'ice::ray :origin (3d-vectors:vec 0 0 0) :direction (3d-vectors:vec 0 0 0))))
+    (is (ice::at r 10) (3d-vectors 0 0 0))))
