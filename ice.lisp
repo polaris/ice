b16646461253052c6ff8efd3c45d0688dd9e05c9
Jan Deinhard <jan.deinhard@gmail.com>
2023-02-04 21:57:43 +0100

    add sphere shading

--- ice.lisp
+++ ice.lisp
@@ -2,19 +2,23 @@
 
 (in-package #:ice)
 
-(defun hit-shpere (center radius r)
+(defun hit-sphere (center radius r)
   (let* ((oc (3d-vectors:v- (get-origin r) center))
 	 (a (3d-vectors:v. (get-direction r) (get-direction r)))
 	 (b (* 2.0 (3d-vectors:v. oc (get-direction r))))
 	 (c (- (3d-vectors:v. oc oc) (* radius radius)))
 	 (discriminant (- (* b b) (* 4 a c))))
-    (> discriminant 0)))
+    (if (< discriminant 0)
+	-1
+	(/ (- (- b) (sqrt discriminant)) (* 2.0 a)))))
 
 (defun ray-color (r)
-  (if (hit-sphere (3d-vectors:vec 0 0 -1) 0.5 r)
-      (3d-vectors:vec 1 0 0)
-      (let ((unit-direction (3d-vectors:vunit (get-direction r))))
-	(let ((tt (* 0.5 (+ (3d-vectors:vy unit-direction) 1))))
+  (let ((tt (hit-sphere (3d-vectors:vec 0 0 -1) 0.5 r)))
+    (if (> tt 0.0)
+	(let ((n (3d-vectors:vunit (3d-vectors:v- (at r tt) (3d-vectors:vec 0 0 -1)))))
+	  (3d-vectors:v* 0.5 (3d-vectors:v+ n (3d-vectors:vec 1 1 1))))
+	(let* ((unit-direction (3d-vectors:vunit (get-direction r)))
+	       (tt (* 0.5 (+ (3d-vectors:vy unit-direction) 1))))
 	  (3d-vectors:v+ (3d-vectors:v* (- 1.0 tt) (3d-vectors:vec 1 1 1)) (3d-vectors:v* tt (3d-vectors:vec 0.5 0.7 1.0)))))))
 
 (defun render (filename image-width)
