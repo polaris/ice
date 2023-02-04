19ccbbcddfcb40be3e03ef5417621e4c559a734b
Jan Deinhard <jan.deinhard@gmail.com>
2023-02-04 20:50:56 +0100

    improve code formatting

--- ice.lisp
+++ ice.lisp
@@ -16,7 +16,8 @@
 	 (origin (3d-vectors:vec 0 0 0))
 	 (horizontal (3d-vectors:vec viewport-width 0 0))
 	 (vertical (3d-vectors:vec 0 viewport-height 0))
-	 (lower-left-corner (3d-vectors:v- (3d-vectors:v- (3d-vectors:v- origin (3d-vectors:v/ horizontal 2)) (3d-vectors:v/ vertical 2)) (3d-vectors:vec 0 0 focal-length))))
+	 (lower-left-corner
+	   (3d-vectors:v- (3d-vectors:v- (3d-vectors:v- origin (3d-vectors:v/ horizontal 2)) (3d-vectors:v/ vertical 2)) (3d-vectors:vec 0 0 focal-length))))
     (with-open-file (output filename
 			    :direction :output
 			    :if-does-not-exist :create
@@ -27,7 +28,8 @@
 	      do (loop for i from 0 below image-width
 		       do (let* ((u (/ i (- image-width 1)))
 				 (v (/ j (- image-height 1)))
-				 (direction (3d-vectors:v- (3d-vectors:v+ (3d-vectors:v+ lower-left-corner (3d-vectors:v* u horizontal)) (3d-vectors:v* v vertical)) origin))
+				 (direction
+				   (3d-vectors:v- (3d-vectors:v+ (3d-vectors:v+ lower-left-corner (3d-vectors:v* u horizontal)) (3d-vectors:v* v vertical)) origin))
 				 (r (make-instance 'ray :origin origin :direction direction))
 				 (pixel-color (ray-color r)))
 			    (write-pixel-color output pixel-color))))))))
