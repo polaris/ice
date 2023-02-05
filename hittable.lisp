95902c3f7c79ea2651105cf58958b5634a6dc251
Jan Deinhard <jan.deinhard@gmail.com>
2023-02-05 18:03:29 +0100

    improve variable naming

--- hittable.lisp
+++ hittable.lisp
@@ -6,13 +6,13 @@
 
 (defgeneric hit (obj r t-min t-max))
 
-(defmethod hit ((list-of-objects cons) (r ray) (t-min number) (t-max number))
+(defmethod hit ((world cons) (r ray) (t-min number) (t-max number))
   (let ((closest-so-far t-max)
         (rec nil))
-    (dolist (item list-of-objects)
-      (let ((temp-rec (hit item r t-min closest-so-far)))
+    (dolist (object world)
+      (let ((temp-rec (hit object r t-min closest-so-far)))
         (if (not (eq temp-rec nil))
             (progn
-             (setf closest-so-far (hit-record-tt temp-rec))
-             (setf rec temp-rec)))))
+              (setf closest-so-far (hit-record-tt temp-rec))
+              (setf rec temp-rec)))))
     rec))
