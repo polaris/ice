4b4320d808cddf1c62d5c3e367479f87c62fda68
Jan Deinhard <jan.deinhard@gmail.com>
2023-02-05 20:50:52 +0100

    improve (render) function definition

--- ice.lisp
+++ ice.lisp
@@ -10,16 +10,12 @@
                (tt (* 0.5 (1+ (vy unit-direction)))))
           (v+ (v* (- 1.0 tt) (vec 1 1 1)) (v* tt (vec 0.5 0.7 1.0)))))))
 
-(defun render (filename image-width)
-  (with-open-file (output filename
-                          :direction :output
-                          :if-does-not-exist :create
-                          :if-exists :overwrite)
-    (let ((world (list (make-instance 'sphere :center (vec 0 0 -1) :radius 0.5)
-                       (make-instance 'sphere :center (vec 0 1 -1) :radius 0.5)
-                       (make-instance 'sphere :center (vec 0 -100.5 -1) :radius 100))))
-      (render-internal output image-width world))))
-
+(defun shoot-ray (i j image-width image-height lower-left-corner horizontal vertical origin world)
+  (let* ((u (/ i (1- image-width)))
+         (v (/ j (1- image-height)))
+         (direction (v- (v+ lower-left-corner (v* u horizontal) (v* v vertical)) origin))
+         (r (make-instance 'ray :origin origin :direction direction)))
+    (ray-color r world)))
 
 (defun render-internal (output image-width world)
   (let* ((aspect-ratio (/ 16.0 9.0))
@@ -32,11 +28,16 @@
          (vertical (vec 0 viewport-height 0))
          (lower-left-corner (v- origin (v/ horizontal 2) (v/ vertical 2) (vec 0 0 focal-length))))
     (format output "P3~%~d ~d~%255~%" image-width image-height)
-    (loop for j from (1- image-height) downto 0
-          do (loop for i from 0 below image-width
-                   do (let* ((u (/ i (1- image-width)))
-                             (v (/ j (1- image-height)))
-                             (direction (v- (v+ lower-left-corner (v* u horizontal) (v* v vertical)) origin))
-                             (r (make-instance 'ray :origin origin :direction direction))
-                             (pixel-color (ray-color r world)))
-                        (write-pixel-color output pixel-color))))))
+    (loop for j from (1- image-height) downto 0 do
+      (loop for i from 0 below image-width do
+        (write-pixel-color output (shoot-ray i j image-width image-height lower-left-corner horizontal vertical origin world))))))
+
+(defun render (filename image-width)
+  (with-open-file (output filename
+                          :direction :output
+                          :if-does-not-exist :create
+                          :if-exists :overwrite)
+    (let ((world (list (make-instance 'sphere :center (vec 0 0 -1) :radius 0.5)
+                       (make-instance 'sphere :center (vec 0 1 -1) :radius 0.5)
+                       (make-instance 'sphere :center (vec 0 -100.5 -1) :radius 100))))
+      (render-internal output image-width world))))
